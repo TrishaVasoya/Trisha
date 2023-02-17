8fd7a6d07791aac5ccbe15c061ac30fa08c4cf52
Trisha Vasoya <rdgitwork@gmail.com>
2023-02-17 12:54:44 -0500

    Using loop for taking input and keeping track of which input has lowest value.

--- f4.cpp
+++ f4.cpp
@@ -10,5 +10,8 @@ int main()
     int n[10], i, min, min_index = 0;//Declare array n with size of 10.
     min = INT_MAX;//Variable min for finding lowest number.
 
+    cout << "Enter the number\n";//Prompting user.
+
+
     return 0;
 }
