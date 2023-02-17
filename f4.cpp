dd9358c441590a1681c4be1148ed4e116ce57408
Trisha Vasoya <rdgitwork@gmail.com>
2023-02-17 12:49:18 -0500

    Declaring and initializing variables.

--- f4.cpp
+++ f4.cpp
@@ -1,3 +1,13 @@
 
 #include<iostream>
 #include<climits>//This is the standard library in c++ which is for the storing and determining the maximumand minimum value.
+
+using namespace std;
+
+/* In main function where main source code is writtten for execute.*/
+int main()
+{
+    int n[10];
+
+    return 0;
+}
