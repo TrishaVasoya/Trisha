f7a620f1a778f1ef728c457489afc98385ab2f22
Trisha Vasoya <rdgitwork@gmail.com>
2023-02-17 12:52:29 -0500

    Informing user about the input.

--- f4.cpp
+++ f4.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main()
 {
     int n[10], i, min, min_index = 0;//Declare array n with size of 10.
+    min = INT_MAX;//Variable min for finding lowest number.
 
     return 0;
 }
